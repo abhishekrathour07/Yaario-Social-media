37564d6ffc77f5f1ae3a2be966d710eedacc2679
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2025-07-13 17:36:31 +0530

    checking the middleware file in deployment

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -6,6 +6,14 @@ export function middleware(request: NextRequest) {
   const isPublicPath = path === '/login' || path === '/register'
   const token = request.cookies.get('auth_token')?.value || ''
 
+  if (path === '/') {
+    if (token) {
+      return NextResponse.redirect(new URL('/home', request.url))
+    } else {
+      return NextResponse.redirect(new URL('/login', request.url))
+    }
+  }
+
   if (isPublicPath && token) {
     return NextResponse.redirect(new URL('/home', request.url))
   }
@@ -22,6 +30,11 @@ export const config = {
     '/register',
     '/home',
     '/profile/:path*',
-    '/messages/:path*'
+    '/messages/:path*',
+    '/friend-requests',
+    '/notifications',
+    '/saved',
+    '/settings',
+    '/varification'
   ]
 }
